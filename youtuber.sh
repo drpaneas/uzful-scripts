301214eca0d0965adc3205ef9e2fceffe417c24f
drpaneas <drpaneas@gmail.com>
2015-03-09 02:19:53 +0100

    optimized version

--- youtuber.sh
+++ youtuber.sh
@@ -3,15 +3,4 @@ echo What is the artist of the song?
 read ARTIST 
 echo What is the name of the song?
 read NAME
-echo "##################################"
-echo "#### P L E A S E W A I T ###"
-echo "##################################"
-mkdir temp && cd temp
-youtube-dl "ytsearch:$ARTIST $NAME album version" 
-ffmpeg -i *.flv -aq 2 "${ARTIST} - ${NAME}.mp3" 
-rm -rf *.flv
-cd ..
-mv temp/*.mp3 $PWD
-rm -rf temp
-mv *.mp3 ~/Music/
-echo Your song has been downloaded successfully
+youtube-dl --extract-audio --audio-format mp3 "ytsearch:$ARTIST $NAME album version" 
